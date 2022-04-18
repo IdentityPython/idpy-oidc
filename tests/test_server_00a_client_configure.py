39d38d494319820e785fc8a41431ae0d760e238b
Roland Hedberg <roland@catalogix.se>
2022-04-18 15:51:38 +0200

    Fixed tests. Blacked and isorted.

--- tests/test_server_00a_client_configure.py
+++ tests/test_server_00a_client_configure.py
@@ -1,11 +1,13 @@
 import json
+import os
 
 from idpyoidc.server import OPConfiguration
 from idpyoidc.server import Server
 from idpyoidc.server.client_configure import verify_oidc_client_information
 from idpyoidc.util import load_config_file
-
 from tests import full_path
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
 extra = {
     "token_usage_rules": {
@@ -23,42 +25,28 @@ extra = {
                 "subject_token_types_supported": [
                     "urn:ietf:params:oauth:token-type:access_token",
                     "urn:ietf:params:oauth:token-type:refresh_token",
-                    "urn:ietf:params:oauth:token-type:id_token"
+                    "urn:ietf:params:oauth:token-type:id_token",
                 ],
                 "requested_token_types_supported": [
                     "urn:ietf:params:oauth:token-type:access_token",
                     "urn:ietf:params:oauth:token-type:refresh_token",
-                    "urn:ietf:params:oauth:token-type:id_token"
+                    "urn:ietf:params:oauth:token-type:id_token",
                 ],
                 "policy": {
                     "urn:ietf:params:oauth:token-type:access_token": {
                         "callable": "/path/to/callable",
-                        "kwargs": {
-                            "audience": ["https://example.com"],
-                            "scopes": ["openid"]
-                        }
+                        "kwargs": {"audience": ["https://example.com"], "scopes": ["openid"]},
                     },
                     "urn:ietf:params:oauth:token-type:refresh_token": {
                         "callable": "/path/to/callable",
-                        "kwargs": {
-                            "resource": ["https://example.com"],
-                            "scopes": ["openid"]
-                        }
+                        "kwargs": {"resource": ["https://example.com"], "scopes": ["openid"]},
                     },
-                    "": {
-                        "callable": "/path/to/callable",
-                        "kwargs": {
-                            "scopes": ["openid"]
-                        }
-                    }
-                }
-            }
+                    "": {"callable": "/path/to/callable", "kwargs": {"scopes": ["openid"]}},
+                },
+            },
         },
         "allowed_scopes": ["scope"],
-        "scopes_to_claims": {
-            "scope_a": ["claim1", "claim2"],
-            "scope_b": []
-        },
+        "scopes_to_claims": {"scope_a": ["claim1", "claim2"], "scope_b": []},
         "add_claims": {
             "always": {
                 "userinfo": ["email", "phone"],
@@ -71,50 +59,50 @@ extra = {
             "by_scope": {
                 "id_token": False,
             },
-        }
-    }
+        },
+    },
 }
 
 
 def test_op_configure_oidc_clients_simple():
-    _str = open(full_path("op_config.json")).read()
+    _str = open(full_path(full_path("op_config.json"))).read()
     _conf = json.loads(_str)
     _conf["oidc_clients"] = {
         "client1": {
             "client_id": "client1",
             "client_secret": "Namnam",
-            "redirect_uris": ['https://openidconnect.net/callback', ''],
-            "response_types": ["code"]
+            "redirect_uris": ["https://openidconnect.net/callback", ""],
+            "response_types": ["code"],
         },
         "client2": {
             "client_id": "client2",
             "client_secret": "spraket",
-            "redirect_uris": ['https://app1.example.net/foo', 'https://app2.example.net/bar'],
-            "response_types": ["code"]
+            "redirect_uris": ["https://app1.example.net/foo", "https://app2.example.net/bar"],
+            "response_types": ["code"],
         },
         "client3": {
             "client_id": "client3",
-            "client_secret": '2222222222222222222222222222222222222222',
-            "redirect_uris": ['https://127.0.0.1:8090/authz_cb/bobcat'],
-            "post_logout_redirect_uri": 'https://openidconnect.net/',
-            "response_types": ["code"]
-        }
+            "client_secret": "2222222222222222222222222222222222222222",
+            "redirect_uris": ["https://127.0.0.1:8090/authz_cb/bobcat"],
+            "post_logout_redirect_uri": "https://openidconnect.net/",
+            "response_types": ["code"],
+        },
     }
 
-    op_conf = OPConfiguration(conf=_conf)
+    op_conf = OPConfiguration(conf=_conf, base_path=BASEDIR)
     assert op_conf
 
 
 def test_verify_oidc_client_information_complext():
-    _conf = load_config_file("op_config.json")
-    server = Server(OPConfiguration(conf=_conf))
+    _conf = load_config_file(full_path("op_config.json"))
+    server = Server(OPConfiguration(conf=_conf, base_path=BASEDIR))
 
     client_conf = {
         "client1": {
             "client_id": "client1",
             "client_secret": "Namnam",
-            "redirect_uris": ['https://openidconnect.net/callback', ''],
-            "response_types": ["code"]
+            "redirect_uris": ["https://openidconnect.net/callback", ""],
+            "response_types": ["code"],
         }
     }
 
@@ -127,14 +115,14 @@ def test_verify_oidc_client_information_complext():
 
 
 def test_verify_oidc_client_information_2():
-    _conf = load_config_file("op_config.json")
-    server = Server(OPConfiguration(conf=_conf))
+    _conf = load_config_file(full_path("op_config.json"))
+    server = Server(OPConfiguration(conf=_conf, base_path=BASEDIR), cwd=full_path(""))
 
     client_conf = {
         "client1": {
             "client_id": "client1",
             "client_secret": "Namnam",
-            "redirect_uris": ['https://openidconnect.net/callback', ''],
+            "redirect_uris": ["https://openidconnect.net/callback", ""],
             "response_types": ["code"],
             "token_usage_rules": {
                 "authorization_code": {
@@ -142,7 +130,7 @@ def test_verify_oidc_client_information_2():
                     "supports_minting": ["access_token", "refresh_token"],
                 },
                 "refresh_token": {"supports_minting": ["access_token"]},
-                "dummy_token": {"supports_minting": ["foobar_token"]}
+                "dummy_token": {"supports_minting": ["foobar_token"]},
             },
         }
     }
