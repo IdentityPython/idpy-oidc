1e2fb9cd02693b862af5a305cdfe27d22719078e
roland <roland@catalogix.se>
2023-03-23 09:45:12 +0100

    Made demo directory. Moved some modules there, renamed. Fixed warnings when running demos.

---
+++ demo/oauth2_add_on_pkce.py
@@ -0,0 +1,182 @@
+import json
+import os
+
+from cryptojwt.key_jar import build_keyjar
+
+from flow import Flow
+from idpyoidc.client.oauth2 import Client
+from idpyoidc.server import Server
+from idpyoidc.server.authz import AuthzHandling
+from idpyoidc.server.client_authn import verify_client
+from idpyoidc.server.configure import ASConfiguration
+from idpyoidc.server.user_authn.authn_context import INTERNETPROTOCOLPASSWORD
+from idpyoidc.server.user_info import UserInfo
+from tests import CRYPT_CONFIG
+from tests import SESSION_PARAMS
+
+KEYDEFS = [
+    {"type": "RSA", "key": "", "use": ["sig"]},
+    {"type": "EC", "crv": "P-256", "use": ["sig"]},
+]
+
+CLIENT_KEYJAR = build_keyjar(KEYDEFS)
+
+COOKIE_KEYDEFS = [
+    {"type": "oct", "kid": "sig", "use": ["sig"]},
+    {"type": "oct", "kid": "enc", "use": ["enc"]},
+]
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+
+def full_path(local_file):
+    return os.path.join(BASEDIR, local_file)
+
+
+USERINFO = UserInfo(json.loads(open(full_path("users.json")).read()))
+
+_OAUTH2_SERVICES = {
+    "metadata": {"class": "idpyoidc.client.oauth2.server_metadata.ServerMetadata"},
+    "authorization": {"class": "idpyoidc.client.oauth2.authorization.Authorization"},
+    "access_token": {"class": "idpyoidc.client.oauth2.access_token.AccessToken"},
+    'resource': {'class': "idpyoidc.client.oauth2.resource.Resource"}
+}
+
+SERVER_CONF = {
+    "issuer": "https://example.com/",
+    "httpc_params": {"verify": False, "timeout": 1},
+    "subject_types_supported": ["public", "pairwise", "ephemeral"],
+    "keys": {"uri_path": "jwks.json", "key_defs": KEYDEFS},
+    "endpoint": {
+        "metadata": {
+            "path": ".well-known/oauth-authorization-server",
+            "class": "idpyoidc.server.oauth2.server_metadata.ServerMetadata",
+            "kwargs": {},
+        },
+        "authorization": {
+            "path": "authorization",
+            "class": "idpyoidc.server.oauth2.authorization.Authorization",
+            "kwargs": {},
+        },
+        "token": {
+            "path": "token",
+            "class": "idpyoidc.server.oauth2.token.Token",
+            "kwargs": {},
+        }
+    },
+    "authentication": {
+        "anon": {
+            "acr": INTERNETPROTOCOLPASSWORD,
+            "class": "idpyoidc.server.user_authn.user.NoAuthn",
+            "kwargs": {"user": "diana"},
+        }
+    },
+    "userinfo": {"class": UserInfo, "kwargs": {"db": {}}},
+    "client_authn": verify_client,
+    "authz": {
+        "class": AuthzHandling,
+        "kwargs": {
+            "grant_config": {
+                "usage_rules": {
+                    "authorization_code": {
+                        "supports_minting": ["access_token", "refresh_token"],
+                        "max_usage": 1,
+                    },
+                    "access_token": {
+                        "supports_minting": ["access_token", "refresh_token"],
+                        "expires_in": 600,
+                    },
+                    "refresh_token": {
+                        "supports_minting": ["access_token"],
+                        "audience": ["https://example.com", "https://example2.com"],
+                        "expires_in": 43200,
+                    },
+                },
+                "expires_in": 43200,
+            }
+        },
+    },
+    "token_handler_args": {
+        "key_conf": {"key_defs": KEYDEFS},
+        "code": {
+            "lifetime": 600,
+            "kwargs": {
+                "crypt_conf": CRYPT_CONFIG
+            }
+        },
+        "token": {
+            "class": "idpyoidc.server.token.jwt_token.JWTToken",
+            "kwargs": {
+                "lifetime": 3600,
+                "add_claims_by_scope": True,
+                "aud": ["https://example.org/appl"],
+            },
+        },
+        "refresh": {
+            "class": "idpyoidc.server.token.jwt_token.JWTToken",
+            "kwargs": {
+                "lifetime": 3600,
+                "aud": ["https://example.org/appl"],
+            },
+        },
+    },
+    "session_params": SESSION_PARAMS,
+    'add_ons': {
+        "pkce": {
+            "function": "idpyoidc.server.oauth2.add_on.pkce.add_support",
+            "kwargs": {},
+        },
+    }
+}
+
+
+server_conf = SERVER_CONF.copy()
+server_conf['add_ons'] = {
+    "pkce": {
+        "function": "idpyoidc.server.oauth2.add_on.pkce.add_support",
+        "kwargs": {},
+    },
+}
+server = Server(ASConfiguration(conf=server_conf, base_path=BASEDIR), cwd=BASEDIR)
+
+CLIENT_CONFIG = {
+    "issuer": SERVER_CONF["issuer"],
+    "client_secret": "SUPERhemligtlösenord",
+    "client_id": "client",
+    "redirect_uris": ["https://example.com/cb"],
+    "client_salt": "salted_peanuts_cooking",
+    "token_endpoint_auth_methods_supported": ["client_secret_post"],
+    "response_types_supported": ["code"],
+    'add_ons': {
+        "pkce": {
+            "function": "idpyoidc.client.oauth2.add_on.pkce.add_support",
+            "kwargs": {
+                "code_challenge_length": 64,
+                "code_challenge_method": "S256"
+            },
+        },
+    }
+}
+
+client = Client(client_type='oauth2', config=CLIENT_CONFIG,
+                keyjar=build_keyjar(KEYDEFS),
+                services=_OAUTH2_SERVICES)
+
+server.context.cdb["client"] = CLIENT_CONFIG
+server.context.keyjar.import_jwks(
+    client.keyjar.export_jwks(), "client")
+
+server.context.set_provider_info()
+
+flow = Flow(client, server)
+msg = flow(
+    [
+        ['server_metadata', 'server_metadata'],
+        ['authorization', 'authorization'],
+        ["accesstoken", 'token']
+    ],
+    scope=['foobar'],
+    server_jwks=server.keyjar.export_jwks(''),
+    server_jwks_uri=server.context.provider_info['jwks_uri']
+)
+assert msg
---
+++ src/idpyoidc/client/oauth2/add_on/jar.py
@@ -0,0 +1,199 @@
+import logging
+from typing import Optional
+
+from idpyoidc.client.oidc.utils import construct_request_uri
+from idpyoidc.client.oidc.utils import request_object_encryption
+from idpyoidc.message.oidc import make_openid_request
+from idpyoidc.time_util import utc_time_sans_frac
+
+logger = logging.getLogger(__name__)
+
+DEFAULT_EXPIRES_IN = 3600
+
+
+def store_request_on_file(service, req, **kwargs):
+    """
+    Stores the request parameter in a file.
+    :param req: The request
+    :param kwargs: Extra keyword arguments
+    :return: The URL the OP should use to access the file
+    """
+    _context = service.upstream_get("context")
+    _webname = _context.get_usage("request_uris")
+    if _webname is None:
+        filename, _webname = construct_request_uri(**kwargs)
+    else:
+        # webname should be a list
+        _webname = _webname[0]
+        filename = _context.filename_from_webname(_webname)
+
+    fid = open(filename, mode="w")
+    fid.write(req)
+    fid.close()
+    return _webname
+
+
+def get_request_object_signing_alg(service, **kwargs):
+    alg = ""
+    for arg in ["request_object_signing_alg", "algorithm"]:
+        try:  # Trumps everything
+            alg = kwargs[arg]
+        except KeyError:
+            pass
+        else:
+            break
+
+    if not alg:
+        _context = service.upstream_get("context")
+        alg = _context.add_on['jar'].get("request_object_signing_alg")
+        if alg is None:
+            alg = "RS256"
+    return alg
+
+
+def construct_request_parameter(service, req, audience=None, **kwargs):
+    """Construct a request parameter"""
+    alg = get_request_object_signing_alg(service, **kwargs)
+    kwargs["request_object_signing_alg"] = alg
+
+    _context = service.upstream_get("context")
+    if "keys" not in kwargs and alg and alg != "none":
+        kwargs["keys"] = service.upstream_get('attribute', 'keyjar')
+
+    if alg == "none":
+        kwargs["keys"] = []
+
+    # This is the issuer of the JWT, that is me !
+    _issuer = kwargs.get("issuer")
+    if _issuer is None:
+        kwargs["issuer"] = _context.get_client_id()
+
+    if kwargs.get("recv") is None:
+        try:
+            kwargs["recv"] = _context.provider_info["issuer"]
+        except KeyError:
+            kwargs["recv"] = _context.issuer
+
+    try:
+        del kwargs["service"]
+    except KeyError:
+        pass
+
+    expires_in = _context.add_on['jar'].get('expires_in', DEFAULT_EXPIRES_IN)
+    if expires_in:
+        req["exp"] = utc_time_sans_frac() + int(expires_in)
+    if _context.add_on['jar'].get('with_jti', False):
+        kwargs['with_jti'] = True
+
+    # Filter out only the arguments I want
+    _mor_args = {
+        k: kwargs[k]
+        for k in [
+            "keys",
+            "issuer",
+            "request_object_signing_alg",
+            "recv",
+            "with_jti",
+            "lifetime",
+        ]
+        if k in kwargs
+    }
+
+    if audience:
+        _mor_args['aud'] = audience
+
+    _req_jwt = make_openid_request(req, **_mor_args)
+
+    if 'target' not in kwargs:
+        kwargs['target'] = _context.provider_info.get("issuer", _context.issuer)
+
+    # Should the request be encrypted
+    _req_jwte = request_object_encryption(_req_jwt, _context,
+                                          service.upstream_get('attribute', 'keyjar'),
+                                          **kwargs)
+    return _req_jwte
+
+
+def jar_post_construct(request_args, service, **kwargs):
+    """
+    Modify the request arguments.
+
+    :param request_args: The request
+    :param service: The service that uses this post_constructor
+    :param kwargs: Extra keyword arguments
+    :return: A possibly modified request.
+    """
+    _context = service.upstream_get("context")
+
+    # Overrides what's in the configuration
+    _request_param = kwargs.get("request_param")
+    _local_dir = ''
+    if _request_param:
+        del kwargs["request_param"]
+    else:
+        _jar_config = _context.add_on['jar']
+        if "request_uri" in _context.add_on['jar']:
+            _request_param = "request_uri"
+            _local_dir = _jar_config.get("requests_dir", "./requests")
+        elif "request_parameter" in _jar_config:
+            _request_param = "request"
+
+    _req = None  # just a flag
+    _state = request_args['state']
+    if _request_param == "request_uri":
+        kwargs["base_path"] = _context.get("base_url") + "/" + "requests"
+        if _local_dir:
+            kwargs["local_dir"] = _local_dir
+        else:
+            kwargs['local_dir'] = kwargs.get('requests_dir', "./requests")
+
+        _req = construct_request_parameter(service, request_args, _request_param, **kwargs)
+        request_args["request_uri"] = store_request_on_file(service, _req, **kwargs)
+    elif _request_param == "request":
+        _req = construct_request_parameter(service, request_args, **kwargs)
+        request_args["request"] = _req
+
+    if _req:
+        _leave = ["request", "request_uri"]
+        _leave.extend(request_args.required_parameters())
+        _keys = [k for k in request_args.keys() if k not in _leave]
+        for k in _keys:
+            del request_args[k]
+
+    _context.cstate.update(_state, request_args)
+
+    return request_args
+
+
+def add_support(service,
+                request_type: Optional[str] = 'request_parameter',
+                request_dir: Optional[str] = '',
+                request_object_signing_alg: Optional[str] = 'RS256',
+                expires_in: Optional[int] = DEFAULT_EXPIRES_IN,
+                with_jti: Optional[bool] = False):
+    """
+    JAR support can only be considered if this client can access an authorization service.
+
+    :param service: Dictionary of services
+    :return:
+    """
+    if "authorization" in service:
+        _service = service["authorization"]
+        _context = _service.upstream_get("context")
+
+        _service.post_construct.append(jar_post_construct)
+        args = {
+            'request_object_signing_alg': request_object_signing_alg,
+            'expires_in': expires_in,
+            'with_jti': with_jti
+        }
+        if request_type == 'request_parameter':
+            args['request_parameter'] = True
+        elif request_type == 'request_uri':
+            args['request_uri'] = True
+            if request_dir:
+                args['request_dir'] = request_dir
+
+        _context.add_on["jar"] = args
+    else:
+        logger.warning("JAR support could NOT be added")
