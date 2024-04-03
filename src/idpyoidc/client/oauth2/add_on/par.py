01f4be6265ef15c092f5b222d7856bdb8b94d77c
roland <roland@catalogix.se>
2024-04-03 17:29:54 +0200

    The pushed authorization implementation deviated from the official RFC (9126). Should be better aligned now.

--- src/idpyoidc/client/oauth2/add_on/par.py
+++ src/idpyoidc/client/oauth2/add_on/par.py
@@ -1,6 +1,5 @@
 import logging
 
-from cryptojwt import JWT
 from cryptojwt.utils import importer
 
 from idpyoidc.client.client_auth import CLIENT_AUTHN_METHOD
@@ -8,9 +7,10 @@ from idpyoidc.message import Message
 from idpyoidc.message.oauth2 import JWTSecuredAuthorizationRequest
 from idpyoidc.server.util import execute
 from idpyoidc.util import instantiate
-from requests import request
 
 logger = logging.getLogger(__name__)
+
+HTTP_METHOD = "POST"
 
 
 def push_authorization(request_args, service, **kwargs):
@@ -24,12 +24,6 @@ def push_authorization(request_args, service, **kwargs):
     method_args = _context.add_on["pushed_authorization"]
     logger.debug(f"PAR method args: {method_args}")
     logger.debug(f"PAR kwargs: {kwargs}")
-
-    if method_args["apply"] is False:
-        return request_args
-
-    _http_method = method_args["http_client"]
-    _httpc_params = service.upstream_get("unit").httpc_params
 
     # Add client authentication if needed
     _headers = {}
@@ -50,29 +44,22 @@ def push_authorization(request_args, service, **kwargs):
             _args["iss"] = _context.issuer
 
         _headers = service.get_headers(
-            request_args, http_method=_http_method, authn_method=authn_method, **_args
+            request_args, http_method=HTTP_METHOD, authn_method=authn_method, **_args
         )
         _headers["Content-Type"] = "application/x-www-form-urlencoded"
 
     # construct the message body
-    if method_args["body_format"] == "urlencoded":
-        _body = request_args.to_urlencoded()
-    else:
-        _jwt = JWT(
-            key_jar=service.upstream_get("attribute", "keyjar"),
-            iss=_context.claims.prefer["client_id"],
-        )
-        _jws = _jwt.pack(request_args.to_dict())
+    _body = request_args.to_urlencoded()
 
-        _msg = Message(request=_jws)
-        for param in request_args.required_parameters():
-            _msg[param] = request_args.get(param)
+    _http_client = method_args.get("http_client", None)
+    if not _http_client:
+        _http_client = service.upstream_get("unit").httpc
 
-        _body = _msg.to_urlencoded()
+    _httpc_params = service.upstream_get("unit").httpc_params
 
     # Send it to the Pushed Authorization Request Endpoint using POST
-    resp = _http_method(
-        method="POST",
+    resp = _http_client(
+        method=HTTP_METHOD,
         url=_context.provider_info["pushed_authorization_request_endpoint"],
         data=_body,
         headers=_headers,
@@ -95,41 +82,30 @@ def push_authorization(request_args, service, **kwargs):
 
 
 def add_support(
-    services,
-    body_format="jws",
-    signing_algorithm="RS256",
-    http_client=None,
-    merge_rule="strict",
-    authn_method="",
+        services,
+        http_client=None,
+        authn_method="",
 ):
     """
     Add the necessary pieces to support Pushed authorization.
 
-    :param merge_rule:
-    :param http_client:
-    :param signing_algorithm:
+    :param http_client: Specification for a HTTP client to use different from the default
+    :param authn_method: The client authentication method to use
     :param services: A dictionary with all the services the client has access to.
-    :param body_format: jws or urlencoded
     """
 
-    if http_client is None:
-        _http_client = request
-    else:
+    if http_client is not None:
         if isinstance(http_client, dict):
             if "class" in http_client:
-                _http_client = instantiate(http_client["class"], **http_client.get("kwargs", {}))
+                http_client = instantiate(http_client["class"], **http_client.get("kwargs", {}))
             else:
-                _http_client = importer(http_client["function"])
+                http_client = importer(http_client["function"])
         else:
-            _http_client = importer(http_client)
+            http_client = importer(http_client)
 
-    _service = services["authorization"]
+    _service = services["authorization"]  # There must be such a service
     _service.upstream_get("context").add_on["pushed_authorization"] = {
-        "body_format": body_format,
-        "signing_algorithm": signing_algorithm,
-        "http_client": _http_client,
-        "merge_rule": merge_rule,
-        "apply": True,
+        "http_client": http_client,
         "authn_method": authn_method,
     }
 
