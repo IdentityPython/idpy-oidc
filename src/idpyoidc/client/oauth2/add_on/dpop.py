904dba768e1414593cebeadca6a0e968ab174060
roland <roland@catalogix.se>
2024-06-14 11:14:05 +0200

    Added more log messages Refactored DPoP support Endpoint specification may come from higher up. Tested scope preference.

--- src/idpyoidc/client/oauth2/add_on/dpop.py
+++ src/idpyoidc/client/oauth2/add_on/dpop.py
@@ -171,11 +171,15 @@ def add_support(services, dpop_signing_alg_values_supported, with_dpop_header=No
     Add the necessary pieces to make pushed authorization happen.
 
     :param services: A dictionary with all the services the client has access to.
-    :param signing_algorithms: Allowed signing algorithms, there is no default algorithms
+    :param dpop_signing_alg_values_supported: Allowed signing algorithms, there is no default algorithms
+    :param with_dpop_header: Which services that should add a DPoP header to a request
     """
 
-    # Access token request should use DPoP header
-    _service = services["accesstoken"]
+    if with_dpop_header is None:
+        with_dpop_header = ["accesstoken", "userinfo"]
+
+    _service = services[with_dpop_header[0]]
+    # Add to Context
     _context = _service.upstream_get("context")
     _algs_supported = [
         alg for alg in dpop_signing_alg_values_supported if alg in get_signing_algs()
@@ -186,20 +190,8 @@ def add_support(services, dpop_signing_alg_values_supported, with_dpop_header=No
     }
     _context.set_preference("dpop_signing_alg_values_supported", _algs_supported)
 
-    _service.construct_extra_headers.append(dpop_header)
-
-    # The same for userinfo requests
-    _userinfo_service = services.get("userinfo")
-    if _userinfo_service:
-        _userinfo_service.construct_extra_headers.append(dpop_header)
-    # To be backward compatible
-    if with_dpop_header is None:
-        with_dpop_header = ["userinfo"]
-
-    # Add dpop HTTP header to these
+    # Add dpop HTTP header to requests by these services
     for _srv in with_dpop_header:
-        if _srv == "accesstoken":
-            continue
         _service = services.get(_srv)
         if _service:
             _service.construct_extra_headers.append(dpop_header)
